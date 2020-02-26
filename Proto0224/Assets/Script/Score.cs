46311911e314cb18592c83fafbb71c65f1b417d4
K-Irim1415 <kitutuki277@gmail.com>
2020-02-26 18:03:31 +0900

    ステージスコア追加

--- Proto0224/Assets/Script/Score.cs
+++ Proto0224/Assets/Script/Score.cs
@@ -7,17 +7,11 @@ public class Score : MonoBehaviour
 {
 	// スコアを表示する
 	public Text scoreText;
-	// ハイスコアを表示する
-	public Text highScoreText;
+	
 
 	// スコア
 	private int score;
 
-	// ハイスコア
-	private int highScore;
-
-	// PlayerPrefsで保存するためのキー
-	private string highScoreKey = "highScore";
 
 	void Start()
 	{
@@ -26,17 +20,13 @@ public class Score : MonoBehaviour
 
 	void Update()
 	{
-		// スコアがハイスコアより大きければ
-		if (highScore < score)
-		{
-			highScore = score;
-		}
+	
 
 		// スコア・ハイスコアを表示する
 		scoreText.text ="Score:"+ score.ToString();
-		highScoreText.text ="High Score"+ highScore.ToString();
+	
 
-		Save();
+	
 
 	}
 
@@ -46,8 +36,7 @@ public class Score : MonoBehaviour
 		// スコアを0に戻す
 		score = 0;
 
-		// ハイスコアを取得する。保存されてなければ0を取得する。
-		highScore = PlayerPrefs.GetInt(highScoreKey, 0);
+		
 	}
 
 	// ポイントの追加
@@ -59,8 +48,7 @@ public class Score : MonoBehaviour
 	// ハイスコアの保存
 	public void Save()
 	{
-		// ハイスコアを保存する
-		PlayerPrefs.SetInt(highScoreKey, highScore);
+		
 		PlayerPrefs.Save();
 
 	
