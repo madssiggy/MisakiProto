22bd22a1d8d11a84772cb6aa38a13f15ebad4996
nimotaku <58283455+nimotaku@users.noreply.github.com>
2020-02-28 14:25:29 +0900

    シーンマネージャー

---
+++ Proto0224/Assets/Script/LEVELscript/SLIMEselect.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SLIMEselect : MonoBehaviour
+{
+    [SerializeField]
+
+    Fade fade = null;
+
+
+
+    public void Fadeout()
+
+    {
+
+
+        fade.FadeIn(1, () => {
+
+            Invoke("LoadScene", 0.5f);
+
+        });
+
+    }
+
+    public void LoadScene()
+    {
+
+        SceneManager.LoadScene("SLIME");
+
+    }
+}
---
+++ Proto0224/Assets/Script/Title.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Title: MonoBehaviour
+{
+    [SerializeField]
+
+    Fade fade = null;
+
+
+
+    public void FadeOut()
+    {
+        //fade.FadeIn (1, () => {
+
+        //	fade.FadeOut (1);
+
+        //});
+
+        fade.FadeIn(1, () =>
+        {
+            Invoke("LoadScene", 0.5f);
+        });
+    }
+
+    public void LoadScene()
+    {
+        SceneManager.LoadScene("LevelScene");
+    }
+
+
+}
